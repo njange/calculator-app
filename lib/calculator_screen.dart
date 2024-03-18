8ec042dd3e2e45bb10d7c06890ee439c585f6a5c
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-19 00:56:48 +0300

    import calculator screen

---
+++ lib/calculator_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalcutorScreenState();
+}
+
+class _CalcutorScreenState extends State<CalculatorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'calculator_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Calculator',
+      theme: ThemeData.dark(),
+      home: CalculatorScreen(),
     );
   }
 }
