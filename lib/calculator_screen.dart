aaea37b03a2821a33ce89fbb3e7df995ccda8b16
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-19 13:19:13 +0300

    scaffold

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -10,6 +10,6 @@ class CalculatorScreen extends StatefulWidget {
 class _CalcutorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
