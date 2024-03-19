5bdd416d4aa3c267ff51290194d6e771ca5275cf
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-03-19 13:56:05 +0300

    trying to solve error

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
@@ -10,6 +11,29 @@ class CalculatorScreen extends StatefulWidget {
 class _CalcutorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body:SafeArea(
+        bottom: false,
+        child:Column(
+          children: [
+            Container(
+              alignment: Alignment.bottomRight,
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                //output display
+                "00000000000000",
+                style: const TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+        //button grid
+      ],
+      )
+      )
+    );
   }
 }
